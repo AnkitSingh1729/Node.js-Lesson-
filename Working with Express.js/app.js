8b7e8009ec430b63d4bd8c80fa2211bb3c7019fd
AnkitSingh1729 <64279281+AnkitSingh1729@users.noreply.github.com>
2021-11-08 17:35:16 +0530

    Handling different routes

--- Working with Express.js/app.js
+++ Working with Express.js/app.js
@@ -1,23 +1,21 @@
-// const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
+app.use('/', (req, res, next) => {
+    console.log('This always runs!');
     next(); // Allows the request to continue to the next middleware in line
 })
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>The "Add Product" Page</h1>');      
+})                                                
+
+app.use('/', (req, res, next) => {
     console.log('In another middleware!');
     res.send('<h1>Hello from Express!</h1>');     // Instead of using res.setHeader() and res.write(), 
 })                                                // we can simply use res.send() now to send the response with express.js  
 
 
-// Older version
-// const server = http.createServer(app);
-// server.listen(3000);
-
-// Using express
 app.listen(3000);
