3429439d31f2170159fbbc42979144d0a7e85e1c
AnkitSingh1729 <64279281+AnkitSingh1729@users.noreply.github.com>
2021-11-09 17:04:29 +0530

    66. adding express router

--- Working with Express.js/app.js
+++ Working with Express.js/app.js
@@ -1,23 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(express.urlencoded( {extended : false}));
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');      
-})    
-
-// This middleware only trigger for incoming post request. It's a filter.
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>');     // Instead of using res.setHeader() and res.write(), 
-})                                                // we can simply use res.send() now to send the response with express.js  
-  
-
+app.use(adminRoutes);
+app.use(shopRoutes);
+ 
 app.listen(3000);
