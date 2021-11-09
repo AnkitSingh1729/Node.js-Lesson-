8fcd0491001332efc093dadbb10231c441d07938
AnkitSingh1729 <64279281+AnkitSingh1729@users.noreply.github.com>
2021-11-09 21:34:49 +0530

    Adding css files and serving them statically

--- Working with Express.js/routes/shop.js
+++ Working with Express.js/routes/shop.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));    // Works on both windows and linux system
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));    // Works on both windows and linux system
 });
 
 module.exports = router;
