74b2d7ce099312963ffc3b011a888f93212eb51e
AnkitSingh1729 <64279281+AnkitSingh1729@users.noreply.github.com>
2021-11-09 17:16:07 +0530

    67. Adding 404 error page

--- Working with Express.js/app.js
+++ Working with Express.js/app.js
@@ -9,5 +9,10 @@ app.use(express.urlencoded( {extended : false}));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>')
+});
+
  
 app.listen(3000);
