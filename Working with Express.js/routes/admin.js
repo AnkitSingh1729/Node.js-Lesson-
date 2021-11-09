ba9e7be4a986d6c06cffb078c7b3b3d30d8085a1
AnkitSingh1729 <64279281+AnkitSingh1729@users.noreply.github.com>
2021-11-09 17:20:28 +0530

    68. Filter Paths

--- Working with Express.js/routes/admin.js
+++ Working with Express.js/routes/admin.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
     res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');      
 })    
 
 // This middleware only trigger for incoming post request. It's a filter.
+// /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
